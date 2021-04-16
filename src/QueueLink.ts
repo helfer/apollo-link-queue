436318bccd4e4845419b0816b409cd3c0c56bc72
Rob Newton <robbienewton@gmail.com>
2021-04-16 14:32:27 -0400

    Added persist & restore, might move to persist pkg

--- src/QueueLink.ts
+++ src/QueueLink.ts
@@ -1,8 +1,11 @@
 import {
     ApolloLink,
+    GraphQLRequest,
     Operation,
     FetchResult,
     NextLink,
+    OperationTypeNode,
+    DocumentNode
 } from '@apollo/client/link/core';
 import {
     Observable,
@@ -18,8 +21,44 @@ interface OperationQueueEntry {
 
 export default class QueueLink extends ApolloLink {
     static listeners: Record< string, ((entry: any) => void)[] > = {};
+    static filter: OperationTypeNode[] = null;
     private opQueue: OperationQueueEntry[] = [];
     private isOpen = true;
+
+    public extract() {
+        // Convert each Operation to a GraphQLRequest so we aren't persisting functions
+        return this.opQueue.map(entry => ({
+            query: entry.operation.query,
+            variables: entry.operation.variables,
+            operationName: entry.operation.operationName,
+            context: entry.operation.getContext(),
+            extensions: entry.operation.extensions,
+        }));
+    }
+
+    public restore(client: ApolloLink, entries: GraphQLRequest[]) {
+        entries.map(graphqlRequest => {
+            const { query, variables, context } = graphqlRequest;
+            if (this.isType(query, 'mutation')) {
+                client.mutate({mutation: query, variables, context});
+            } else {
+                client.query({query, variables, context});
+            }
+        })
+    }
+
+    private isType(query: DocumentNode, type: OperationTypeNode) {
+        return query.definitions.filter((e) => {
+            return (e as any).operation === type
+        }).length > 0;
+    }
+
+    private isFilteredOut(operation: Operation) {
+        if (!QueueLink.filter || !QueueLink.filter.length) return false;
+        return operation.query.definitions.filter((e) => {
+            return QueueLink.filter.includes((e as any).operation)
+        }).length > 0;
+    }
 
     public open() {
         this.isOpen = true;
@@ -46,6 +85,10 @@ export default class QueueLink extends ApolloLink {
         QueueLink.listeners = { ...QueueLink.listeners, ...newListener };
     };
 
+    public static setFilter = (filter: OperationTypeNode[]) => {
+        QueueLink.filter = filter;
+    };
+
     private static key(op: string, ev: string) {
         return `${op}${ev}`.toLocaleLowerCase();
     }
@@ -59,6 +102,9 @@ export default class QueueLink extends ApolloLink {
             return forward(operation);
         }
         if (operation.getContext().skipQueue) {
+            return forward(operation);
+        }
+        if (this.isFilteredOut(operation)) {
             return forward(operation);
         }
         return new Observable<FetchResult>((observer: Observer<FetchResult>) => {
