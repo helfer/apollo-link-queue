584c26d22dbb3568a28d807e58a67aaffe875ec6
remote <fishyFrogFace@users.noreply.github.com>
2021-04-29 10:31:30 +0200

    Removal of event listeners (#2)
    * Removal of old listeners
    
    - Return a GUID on creation of a listener
    - Function for removing the listener by passing GUID, opName and event
    
    * Remove tslint rules that are no longer in use

--- src/QueueLink.ts
+++ src/QueueLink.ts
@@ -1,84 +1,153 @@
 import {
-    ApolloLink,
-    Operation,
-    FetchResult,
-    NextLink,
+  ApolloLink,
+  Operation,
+  FetchResult,
+  NextLink,
 } from '@apollo/client/link/core';
-import {
-    Observable,
-    Observer,
-} from '@apollo/client/utilities';
+import { Observable, Observer } from '@apollo/client/utilities';
+import { createGuid } from './Utils';
 
 interface OperationQueueEntry {
-    operation: Operation;
-    forward: NextLink;
-    observer: Observer<FetchResult>;
-    subscription?: { unsubscribe: () => void };
+  operation: Operation;
+  forward: NextLink;
+  observer: Observer<FetchResult>;
+  subscription?: { unsubscribe: () => void };
+}
+
+type event = 'dequeue' | 'enqueue' | 'change';
+
+interface Listener {
+  id: string;
+  callback: (entry: any) => void;
 }
 
 export default class QueueLink extends ApolloLink {
-    static listeners: Record< string, ((entry: any) => void)[] > = {};
-    private opQueue: OperationQueueEntry[] = [];
-    private isOpen = true;
+  static listeners: Record<string, Listener[]> = {};
+  private opQueue: OperationQueueEntry[] = [];
+  private isOpen = true;
 
-    public open() {
-        this.isOpen = true;
-        this.opQueue.forEach(({ operation, forward, observer }) => {
-            const key: string = QueueLink.key(operation.operationName, 'dequeue');
-            if (key in QueueLink.listeners) {
-                QueueLink.listeners[key].forEach((listener) => {
-                    listener({ operation, forward, observer });
-                });
-            }
-            forward(operation).subscribe(observer);
-        });
-        this.opQueue = [];
+  public clear() {
+    this.opQueue = [];
+    QueueLink.listeners = {};
+  }
+
+  public open() {
+    this.isOpen = true;
+
+    const first: OperationQueueEntry | undefined = this.opQueue.shift();
+
+    if (first !== undefined) {
+      const { operation, forward, observer } = first;
+
+      this.triggerListeners(first, 'dequeue');
+
+      forward(operation).subscribe(
+        (value) => {
+          if (observer && observer.next) {
+            observer?.next(value);
+          }
+        },
+        (error) => {
+          if (observer && observer.error) {
+            observer?.error(error);
+          }
+          this.open();
+        },
+        () => {
+          if (observer && observer.complete) {
+            observer?.complete();
+          }
+          this.open();
+        }
+      );
     }
+  }
 
-    public static addLinkQueueEventListener = (opName: string, event: 'dequeue' | 'enqueue', listener: (entry: any) => void) => {
-        const key: string = QueueLink.key(opName, event);
+  public static addLinkQueueEventListener = (
+    opName: string,
+    event: event,
+    callback: (entry: any) => void
+  ) => {
+    if (event === 'change') opName = '';
+    const key: string = QueueLink.key(opName, event);
 
-        const newListener = { [key]: [
-            ...(key in QueueLink.listeners ? QueueLink.listeners[key] : []),
-            ...[listener], ]
-        };
+    const newGuid = createGuid();
 
-        QueueLink.listeners = { ...QueueLink.listeners, ...newListener };
+    const newListener = {
+      [key]: [
+        ...(key in QueueLink.listeners ? QueueLink.listeners[key] : []),
+        ...[{ id: newGuid, callback }],
+      ],
     };
 
-    private static key(op: string, ev: string) {
-        return `${op}${ev}`.toLocaleLowerCase();
-    }
-    public close() {
-        this.isOpen = false;
-    }
+    QueueLink.listeners = { ...QueueLink.listeners, ...newListener };
 
-    public request(operation: Operation, forward: NextLink) {
-        if (this.isOpen) {
-            return forward(operation);
-        }
-        if (operation.getContext().skipQueue) {
-            return forward(operation);
-        }
-        return new Observable<FetchResult>((observer: Observer<FetchResult>) => {
-            const operationEntry = { operation, forward, observer };
-            this.enqueue(operationEntry);
-            return () => this.cancelOperation(operationEntry);
-        });
-    }
+    return newGuid;
+  };
 
-    private cancelOperation(entry: OperationQueueEntry) {
-        this.opQueue = this.opQueue.filter(e => e !== entry);
-    }
+  public static removeLinkQueueEventListener = (
+    opName: string,
+    event: event,
+    id: string
+  ) => {
+    if (event === 'change') opName = '';
+    const key: string = QueueLink.key(opName, event);
 
-    private enqueue(entry: OperationQueueEntry) {
-        this.opQueue.push(entry);
+    if (QueueLink.listeners[key] !== undefined) {
+      QueueLink.listeners[key] = QueueLink.listeners[key].filter(
+        (listener) => listener.id !== id
+      );
 
-        const key: string = QueueLink.key(entry.operation.operationName, 'enqueue');
-        if (key in QueueLink.listeners) {
-            QueueLink.listeners[key].forEach((listener) => {
-                listener(entry);
-            });
-        }
+      if (QueueLink.listeners[key].length === 0) {
+        delete QueueLink.listeners[key];
+      }
     }
+  };
+
+  public close() {
+    this.isOpen = false;
+  }
+
+  public request(operation: Operation, forward: NextLink) {
+    if (this.isOpen) {
+      return forward(operation);
+    }
+    if (operation.getContext().skipQueue) {
+      return forward(operation);
+    }
+    return new Observable<FetchResult>((observer: Observer<FetchResult>) => {
+      const operationEntry = { operation, forward, observer };
+      this.enqueue(operationEntry);
+      return () => this.cancelOperation(operationEntry);
+    });
+  }
+
+  private static key(op: string, ev: string) {
+    return `${op}${ev}`.toLocaleLowerCase();
+  }
+
+  private cancelOperation(entry: OperationQueueEntry) {
+    this.opQueue = this.opQueue.filter((e) => e !== entry);
+  }
+
+  private enqueue(entry: OperationQueueEntry) {
+    this.opQueue.push(entry);
+
+    this.triggerListeners(entry, 'enqueue');
+  }
+
+  private triggerListeners(entry: OperationQueueEntry, event: string) {
+    let key: string = QueueLink.key(entry.operation.operationName, event);
+    if (key in QueueLink.listeners) {
+      QueueLink.listeners[key].forEach((listener) => {
+        listener.callback(entry);
+      });
+    }
+    key = QueueLink.key('', 'change');
+    if (key in QueueLink.listeners) {
+      QueueLink.listeners[key].forEach((listener) => {
+        listener.callback(this.opQueue);
+      });
+    }
+  }
 }
---
+++ src/Utils.ts
@@ -0,0 +1,7 @@
+export const createGuid = () => {
+  function _p8(s: boolean) {
+    const p = (Math.random().toString(16) + '000000000').substr(2, 8);
+    return s ? '-' + p.substr(0, 4) + '-' + p.substr(4, 4) : p;
+  }
+  return _p8(false) + _p8(true) + _p8(true) + _p8(false);
+};
